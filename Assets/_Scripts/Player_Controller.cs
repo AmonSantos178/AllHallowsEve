7569a345a803cc4cf8b1e06c865d323a104e9933
AmonSantos <Amonsantos178@gmail.com>
2022-01-22 16:47:09 -0300

    Animation Test and basic movement proved successfull

---
+++ Assets/_Scripts/Player_Controller.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    //Hello! If you're reading this, I'd like to thank you and everyone over at LSW for the amazing opportunity! :)
+    #region Variables
+    float speed = 3f;
+    Animator anim;
+    Rigidbody2D rb;
+
+
+    #endregion
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Here, i've separately programmed each key, as to avoid diagonal movement so that the animation matches    
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector3(0, speed, 0);
+            anim.SetInteger("Direction", 2);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector3(0, -speed, 0);
+            anim.SetInteger("Direction", 1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+            anim.SetInteger("Direction", 3);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector3(-speed, 0);
+            anim.SetInteger("Direction", 4);
+
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            anim.SetInteger("Direction", 0);
+        }
+    }
+}
