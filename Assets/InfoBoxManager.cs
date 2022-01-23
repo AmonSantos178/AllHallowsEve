61646616418d3fb912136b09dd6bc87dd2417c46
AmonSantos <Amonsantos178@gmail.com>
2022-01-23 06:38:41 -0300

    Finished Interaction framework for all dialogues

--- Assets/InfoBoxManager.cs
+++ Assets/InfoBoxManager.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -7,10 +6,15 @@ public class InfoBoxManager : MonoBehaviour
 {
     [SerializeField] Text infoBarText;
     float interactionDistance = 10f;
+    InteractionManager im;
+    PlayerController pc;
+    [SerializeField] GameObject fisherman;
 
     void Start()
     {
         infoBarText.text = "This is your information bar. Right-Click something to learn about it!";
+        im = FindObjectOfType<InteractionManager>();
+        pc = FindObjectOfType<PlayerController>();
     }
 
     void Update()
@@ -25,7 +29,7 @@ public class InfoBoxManager : MonoBehaviour
             if (hit.collider != null)
             {
                 float distance = CheckDistance(new Vector2(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y));
-                if(distance >= interactionDistance)
+                if (distance >= interactionDistance)
                 {
                     infoBarText.text = "That's too far away for me to interact. I should get closer.";
                 }
@@ -99,32 +103,157 @@ public class InfoBoxManager : MonoBehaviour
     {
         switch (tag)
         {
-            case "Bard": return "This is the Bard's House, the music he's playing gives it away.";
-            case "Tesker": return "This is the Tesker household. Mr. Tesker, the fisherman, lives here with his daughter.";
-            case "Hobbit": return "Martin Holm, the author, lives here. He does not appreciate visitors.";
-            case "Candy": return "Lady Hershell sells the best sweets in the province. I wonder where she gets them.";
-            case "Costume": return "The mayor's wife, Lady Helen, works here selling clothes. She is not fond of me.";
-            case "Decor": return "Sweet Rosalyn works here, selling decorations. I often swing by to chat.";
-            case "Fisherman": return "This is Old Mr. Tesker, our fisherman. He's grumpy, but he's a good man.";
-            case "Food": return "This is Ms. Nicole's Food stand. Everything always looks so appetizing.";
-            case "Mayor": return "Mayor Linday's house. Both him and his wife are at work, I doubt there's anyone here.";
-            case "Grave": return "The Gravedigger's home. I shouldn't bother him, he's always jumpy on Halloween.";
-            case "Hall": return "The town hall. Mayor Lindsay told me not to return here until everything is done.";
-            case "Mom": return "That's my house. I'm pretty sure mom's home, maybe she can help.";
-            case "Friend": return "That's Sam's house. He's my best friend ever since we were little.";
-            case "Stan": return "Stan's house. He's a bit weird, but certainly the smartest guy in the village.";
-            case "Willy": return "Old Willie's house. He always cracks me up, but I doubt he's awake right now.";
-            case "Empty": return "This house has been empty for years. The only things inside are racoons and spiders.";
-            case "Farm": return "Mr. Holm's Farm. His nephew takes care of it, mostly, but he's away on a journey.";
-            case "Graveyard": return "This is our graveyard. This place is going to be full of drunks tonight, I'm sure.";
+            case "Bard":
+                if (im.hiredBard)
+                {
+                    infoBarText.text = "Looks like he's already left home to go play at the party. Great, one less worry!"; return;
+                }
+                else if(im.hasFlowers)
+                {
+                    im.hiredBard = true;
+                    return;
+                }
+                else return;
+            case "Tesker":
+                if (im.stanQuest)
+                {
+                    infoBarText.text = "Emily isn't answering the door. Perhaps Stan's gift worked..."; return;
+                }
+                else if (im.emilyQuest)
+                {
+                    infoBarText.text = "Emily isn't answering the door. Perhaps Stan's gift worked..."; return;
+                }
+                else if (im.hasLunch)
+                {
+                    infoBarText.text = "I should deliver her father's lunch before I talk to Emily Again"; return;
+                }
+                else if (im.hasGifts)
+                {
+                    im.hasGifts = false;
+                    infoBarText.text = "Emily isn't answering the door. Perhaps Stan's gift worked..."; return;
+                }
+                else return;
+            case "Hobbit":
+                if (im.cheeredMartin)
+                {
+                    infoBarText.text = "Looks like he's left his house for his final adventure."; return;
+                }
+                else if (pc.CheckTag() == "C2")
+                {
+                    im.cheeredMartin = true;
+                    return;
+                }
+                else
+                {
+                    infoBarText.text = "'No, thank you! I don't want any more visitors, well-wishers or distant relations!'"; return;
+                }
+            case "Candy": return;
+            case "Costume": return;
+            case "Decor": return;
+            case "Fisherman":
+                if (pc.CheckTag() == "C5")
+                {
+                    Destroy(fisherman.gameObject);
+                    return;
+                }
+                else if (im.hasLunch)
+                {
+                    im.emilyQuest = true;
+                    return;
+                }
+                else return;
+            case "Food": return;
+            case "Mayor": infoBarText.text = "No one answered the door. As I suspected, they're both at work."; return;
+            case "Grave":
+                if (im.scaredDermot)
+                {
+                    infoBarText.text = "Looks like he won't open the door. I must have really scared him.";
+                    return;
+                }
+                else if (pc.CheckTag() == "C4")
+                {
+                    im.scaredDermot = true;
+                    return;
+                }
+                else if(im.hasCandles)
+                {
+                    im.gravediggerQuest = true;
+                    return;
+                }
+                else return;
+            case "Hall":
+                if (im.CheckGameCompletion() == true)
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            case "Mom":
+                if (im.momQuest)
+                {
+                    infoBarText.text = "No answer. Maybe she left to go egg some houses.";
+                    return;
+                }
+                else if (im.hasEggs)
+                {
+                    im.momQuest = true;
+                    return;
+                }
+                else return;
+            case "Friend":
+                if (im.friendQuest)
+                {
+                    infoBarText.text = "No answer. Perhaps he left home early for the party. I'll see him there later.";
+                    return;
+                }
+                else if (im.hasCostume)
+                {
+                    im.friendQuest = true;
+                    return;
+                }
+                else return;
+            case "Stan":
+                if (im.stanQuest)
+                {
+                    return;
+                }
+                else if (im.hasGifts)
+                {
+                    infoBarText.text = "I should deliver the gifts before I talk to Stan again.";
+                    return;
+                }
+                else return;
+            case "Willy":
+                if (im.cheeredWillie)
+                {
+                    infoBarText.text = "Let him rest. He's a good man with a heart of gold.";
+                    return;
+                }
+                else if (pc.CheckTag() == "C1")
+                {
+                    im.cheeredWillie = true;
+                    return;
+                }
+                else if (im.hasFishySticks)
+                {
+                    im.gravediggerQuest = true;
+                    return;
+                }
+                else return;
+
+            case "Empty": infoBarText.text = "I knock, but hear no one. It's probably best to stay away from here."; return;
+            case "Farm": infoBarText.text = "I shouldn't mess with Mr. Holm's farm. I hear he has some powerful friends."; return;
+            case "Graveyard": infoBarText.text = "I'm not digging in there. YOU dig in there. I refuse."; return;
             case "Barrier": infoBarText.text = "If I take one more step, it'll be the farthest away from home I've ever been."; return;
             case "Water": infoBarText.text = "Sorry, I can't swim. And I don't feel like learning the hard way."; return;
             default: return;
         }
     }
 
-    void CheckIfScared(string tag)
+    /*void CheckState(string tag)
     {
-
-    }
+        im.GiveState(tag);
+    }*/
 }
--- Assets/InteractionManager.cs
+++ Assets/InteractionManager.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
-    bool scaredTesker;
-    bool scaredDermot;
-    bool cheeredWillie;
-    bool cheeredMartin;
+    public bool scaredTesker;
+    public bool scaredDermot;
+    public bool cheeredWillie;
+    public bool cheeredMartin;
 
-    bool hasEggs;
-    bool hasFlowers;
-    bool hasLunch;
-    bool hasCostume;
-    bool hasCandles;
-    bool hasMilk;
-    bool hasFishySticks;
-    bool hasGifts;
+    public bool hasFlowers;
+    public bool hasLunch;
+    public bool hasCostume;
+    public bool hasCandles;
+    public bool hasEggs;
+    public bool hasFishySticks;
+    public bool hasGifts;
 
-    bool hasDecorations;
-    bool hiredBard;
+    public bool emilyQuest;
+    public bool friendQuest;
+    public bool momQuest;
+    public bool gravediggerQuest;
+    public bool stanQuest;
 
-    int sweets;
+    public bool hasDecorations;
+    public bool hiredBard;
+
+    public int sweets;
 
 
     string costume;
@@ -37,5 +42,48 @@ public class InteractionManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public bool GiveState(string tag)
+    {
+        switch (tag)
+        {
+            case "Bard": return hiredBard;
+            case "Hobbit": return cheeredMartin;
+            case "Fisherman": return hasLunch;
+             case "Grave": return scaredDermot;
+            case "Hall": return CheckGameCompletion();
+            case "Mom": return hasEggs;
+            case "Friend": return hasCostume;
+            case "Stan": return hasGifts;
+            case "Willy": return cheeredWillie;
+            default: return false;
+        }
+    }
+
+    public bool CheckGameCompletion()
+    {
+        if (hasDecorations && hiredBard)
+        {
+            if (hasCostume && sweets >= 2)
+            {
+                return true;
+            }
+            else return false;
+        }
+        else return false;
+    }
+
+    public bool CheckQuestCompletion(string tag)
+    {
+        switch (tag)
+        {
+            case "Emily": return emilyQuest;
+            case "Grave": return gravediggerQuest;
+            case "Mom": return momQuest;
+            case "Friend": return friendQuest;
+            case "Stan": return stanQuest;
+            default: return false;
+        }
     }
 }
